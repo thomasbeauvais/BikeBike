c3c3213d29d60414f233c7693ccdf99ece23d7b4
Godwin <goodgodwin@hotmail.com>
2013-07-11 21:28:50 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BikeBike::Application.config.secret_key_base = '0d1bc6ee38d362261b0a62ad37ff55b840d1eeee54266d3f11f90db8748ea121c75a7b558842f5c199f04a91a946251f5230c8d24555f8c9ec918f36e2b2fb9a'
